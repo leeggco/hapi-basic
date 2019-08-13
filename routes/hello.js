6977dd023cfd29a56361743fbd3e671d9cac583b
leeggco <leeggco@gmail.com>
2019-08-13 17:47:36 +0800

    # 静态页面

--- routes/hello.js
+++ routes/hello.js
@@ -8,4 +8,16 @@ module.exports = [{
     tags: ['api', 'users'],
     description: '测试hello',
   },
+}, {
+  method: 'GET',
+  path: '/hello',
+  handler: (request, h) => {
+    return h.file('./views/hello.html')
+  }
+}, {
+  method: 'GET',
+  path: '/epub',
+  handler: (request, h) => {
+    return h.file('./views/epub.html')
+  }
 }]
